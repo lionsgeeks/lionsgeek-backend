d660571377b4545fc1491714130168a18accd443
forkanimahdi <forkanimahdi@gmail.com>
2024-11-09 15:19:14 +0100

    adjust ui when no messag received  in the cntact form (empty inbox)

--- resources/views/contacts/contacts.blade.php
+++ resources/views/contacts/contacts.blade.php
@@ -7,40 +7,51 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+            @if ($contacts->count() > 0)
+                <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
 
-                    <table class="w-full">
-                        <thead>
-                            <th>Name</th>
-                            <th>Phone</th>
-                            <th>Email</th>
-                            <th>Message</th>
-                        </thead>
+                        <table class="w-full">
+                            <thead>
+                                <th>Name</th>
+                                <th>Phone</th>
+                                <th>Email</th>
+                                <th>Message</th>
+                            </thead>
 
-                        <tbody class="w-full">
-                            @foreach ($contacts as $contact)
-                                <tr class="w-full text-center h-[5vh]">
+                            <tbody class="w-full">
+                                @foreach ($contacts as $contact)
+                                    <tr class="w-full text-center h-[5vh]">
 
-                                    <td>
-                                        {{ $contact->full_name }}
-                                    </td>
-                                    <td>
-                                        {{ $contact->phone }}
-                                    </td>
-                                    <td>
-                                        {{ $contact->email }}
-                                    </td>
-                                    <td>
-                                        {{ $contact->message }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                        <td>
+                                            {{ $contact->full_name }}
+                                        </td>
+                                        <td>
+                                            {{ $contact->phone }}
+                                        </td>
+                                        <td>
+                                            {{ $contact->email }}
+                                        </td>
+                                        <td>
+                                            {{ $contact->message }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
 
+                    </div>
+                </div>
+            @else
+            <div class="h-[70vh] bg-white rounded-lg flex items-center justify-center w-full">
+                <div class="text-center">
+                    <h1 class="text-2xl font-semibold text-gray-700 mb-3">No Messages Received</h1>
+                    <p class="text-gray-500 mb-6">It seems there are no messages in your inbox yet.</p>
+                    <p class="text-gray-400">Encourage visitors to reach out by checking that your contact form is accessible and easy to use.</p>
                 </div>
             </div>
+            
+            @endif
         </div>
     </div>
 </x-app-layout>

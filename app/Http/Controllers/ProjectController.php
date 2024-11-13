6be6c2f10332be07dc1da6e976189fa3e80eeeb1
forkanimahdi <forkanimahdi@gmail.com>
2024-11-13 15:00:07 +0100

    project translation with gemini

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Carbon\Carbon;
+use Gemini;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ProjectController extends Controller
 {
@@ -16,6 +18,11 @@ class ProjectController extends Controller
     {
         //
         $projects = Project::all();
+        $projects->map(function ($project) {
+            $project->description = json_decode($project->description);
+            $project->description = $project->description->en;
+        });
+
         return view("project.project", compact("projects"));
     }
 
@@ -38,24 +45,42 @@ class ProjectController extends Controller
             "description" => "required",
             "logo" => "required|mimes:png,jpg,svg,jfif,gif",
         ]);
-        $logo = file_get_contents($request->logo);
-        $logoName = hash("sha256", $logo . Carbon::now())  . "." . $request->logo->getClientOriginalExtension();
-        Storage::disk("public")->put("images/projects/" . $logoName, $logo);
 
+
+
+        if ($request->hasFile("logo")) {
+            $logo = file_get_contents($request->logo);
+            $logoName = hash("sha256", $logo . Carbon::now())  . "." . $request->logo->getClientOriginalExtension();
+            Storage::disk("public")->put("images/projects/" . $logoName, $logo);
+        }
         if ($request->hasFile("preview")) {
             $preview = file_get_contents($request->preview);
             $previewName = hash("sha256", $preview . Carbon::now())  . "." . $request->preview->getClientOriginalExtension();
             Storage::disk("public")->put("images/projects/" . $previewName, $preview);
         }
 
-        Project::create([
-            "name" => $request->name,
-            "description" => $request->description,
-            "logo" => $logoName,
-            "preview" => $request->hasFile("preview") ? $previewName : null
-        ]);
+        $gemini_api_key = env("GEMINI_API_KEY");
 
-        return back()->with("success", "kj");
+        $client = Gemini::client($gemini_api_key);
+
+        $prompt = "Translate the following text into English, French, and Arabic, and return the result in an object like this  one  all in one  line: 
+        {\"en\": \"[translated text in English]\", \"fr\": \"[translated text in French]\", \"ar\": \"[translated text in Arabic]\"}. 
+        The text to translate is: \"$request->description\"";
+
+        try {
+            $result = $client->geminiPro()->generateContent($prompt);
+            // dd($result->text());
+            Project::create([
+                "name" => $request->name,
+                "description" => $result->text(),
+                "logo" => $logoName,
+                "preview" => $request->hasFile("preview") ? $previewName : null
+            ]);
+
+            return back()->with("success", "kj");
+        } catch (\Throwable $th) {
+            return back();
+        }
     }
 
     /**
@@ -100,9 +125,19 @@ class ProjectController extends Controller
             Storage::disk("public")->put("images/projects/" . $previewName, $preview);
         }
 
+        $gemini_api_key = env("GEMINI_API_KEY");
+
+        $client = Gemini::client($gemini_api_key);
+
+
+        $prompt = "Translate the following text into English, French, and Arabic, and return the result in an object like this  one  all in one  line: 
+        {\"en\": \"[translated text in English]\", \"fr\": \"[translated text in French]\", \"ar\": \"[translated text in Arabic]\"}. 
+        The text to translate is: \"$request->description\"";
+        $result = $client->geminiPro()->generateContent($prompt);
+
         $project->update([
             "name" => $request->name,
-            "description" => $request->description,
+            "description" => $result->text(),
             "logo" => $request->hasFile("logo") ? $logoName : $project->logo,
             "preview" => $request->hasFile("preview") ? $previewName : $project->preview
         ]);

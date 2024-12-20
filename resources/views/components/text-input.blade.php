d877982bc398e7f24ad84a1e74d6792e5a3443d3
walid-cadi <cadiwalid02@gmail.com>
2024-12-20 12:12:47 +0100

    edit style in profile page

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-100 dark:text-gray-700 focus:border-gray-700 dark:focus:border-2 dark:focus:border-gray-700 focus:ring-0  dark:focus:ring-0 rounded-md shadow-sm']) }}>

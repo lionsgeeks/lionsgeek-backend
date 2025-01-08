8140d3b88a6e21f90af22fc7e840b57543c1a0e3
walid-cadi <cadiwalid02@gmail.com>
2025-01-08 13:04:42 +0100

    edit in message flash

--- app/Http/Controllers/InfoSessionController.php
+++ app/Http/Controllers/InfoSessionController.php
@@ -53,7 +53,7 @@ class InfoSessionController extends Controller
             'start_date' => $request->start_date,
             'places' => $request->places,
         ]);
-        return back();
+        return back()->with('success','Session Has Been Created Successfully');
     }
     public function update(Request $request,  $id)
     {
